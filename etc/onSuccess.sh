ecc5d9436d04041a372f533e7391e67ff26f99c1
Derek Haynes <dhaynes@redhat.com>
2021-09-16 10:19:31 +0200

    Remove TravisCI config

--- etc/onSuccess.sh
+++ etc/onSuccess.sh
@@ -3,7 +3,7 @@
 echo "after_success script"
 
 if [ "$BRANCH" == "master" ] && [ "$JDK_VERSION" == "openjdk7" ] && [ "$PULL_REQUEST" == "false" ]; then
-    echo "DEPLOY TRAVIS BUILD"
+    echo "DEPLOY BUILD"
     echo "Current directory is $(pwd)"
     mvn clean deploy -DskipTests;
 fi
